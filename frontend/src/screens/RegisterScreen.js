7510f06fee40b3900440f52419cb89b8999f3bed
CodeKazuko <hillery262627@gmail.com>
2021-05-20 19:24:04 +0800

    heroku publish

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -30,109 +30,89 @@ const RegisterScreen = {
       })
   },
   render: () => `
-  <script type="text/javascript"> 
-  function matchTest(){
-    let password = document.querySelector('password').value
-    let confirmPassword = document.querySelector('re-password').value
-    
-    if(password !== confirmPassword){
-      console.log("Password don't match. Please try again.")
-      return false
-    }
-    else if(password === confirmPassword){
-      console.log("Password match")
-    }
-  }
-  repeatPassword.addEventListener("onkeyup", (e) => {
-    matchTest()
-   })
-
-  window.onload = function() {
-  let email = document.getElementById("email")
-  let password = document.getElementById("password")
-  
-  
-  email.addEventListener('input',()=>{
-    let emailBox = document.querySelector('.emailBox')
-    let emailText = document.querySelector('.emailText')
-    const emailPattern = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$/
-
-    if(email.value.match(emailPattern)){
-      emailBox.classList.add('valid')
-      emailBox.classList.remove('invalid')
-      emailText.innerHTML = "Your Email Address in Valid"
-      alert("Your Email Address in Valid")
-    }else{
-      emailBox.classList.add('invalid')
-      emailBox.classList.remove('valid')
-      emailText.innerHTML = "Must be a valid email address."
-      alert("Your Email Address in invalid")
-    }
-   })
-   
-  password.addEventListener('input',()=>{
-      let passBox = document.querySelector('.passBox');
-      let passText = document.querySelector('.passText');
-      const passPattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
-
-      if(password.value.match(passPattern)){
-         passBox.classList.add('valid');
-         passBox.classList.remove('invalid');
-         passText.innerHTML = "Your Password in Valid"; 
-         alert("Your Password is Valid")
-      }else{
-         passBox.classList.add('invalid');
-         passBox.classList.remove('valid');
-         passText.innerHTML = "Your password must be at least 8 characters as well as contain at least one uppercase, one lowercase, and one number."; 
-         alert("Your Password is invalid")
-    }
-  })   
-}
-</script>    
-      <div class="form">
-        <form id="register-form">
+          <script type="text/javascript"> 
+          window.onload = function() {
+            let email = document.getElementById("email")
+            let password = document.getElementById("password")
+            let repeatPassword = document.getElementById("re-password")
+          
+            email.addEventListener("input", () => {
+              let emailBox = document.querySelector(".emailBox")
+              let emailText = document.querySelector(".emailText")
+              const emailPattern = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$/
+          
+              if (email.value.match(emailPattern)) {
+                emailBox.classList.add("valid")
+                emailBox.classList.remove("invalid")
+                emailText.innerHTML = "Your Email Address in Valid"
+              } else {
+                emailBox.classList.add("invalid")
+                emailBox.classList.remove("valid")
+                emailText.innerHTML = "Must be a valid email address."
+              }
+            })
+          
+            password.addEventListener("input", () => {
+              let passBox = document.querySelector(".passBox")
+              let passText = document.querySelector(".passText")
+              const passPattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
+          
+              if (password.value.match(passPattern)) {
+                passBox.classList.add("valid")
+                passBox.classList.remove("invalid")
+                passText.innerHTML = "Your Password in Valid"
+              } else {
+                passBox.classList.add("invalid")
+                passBox.classList.remove("valid")
+                passText.innerHTML =
+                  "Your password must be at least 8 characters as well as contain at least one uppercase, one lowercase, and one number."
+              }
+            });
+          
+            function matchTest() {
+              let pass = password.value
+              let confirmPass = repeatPassword.value
+          
+              if (pass !== confirmPass) {
+                console.log("Password don't match. Please try again.")
+                return false
+              } else {
+                console.log("Password match")
+              }
+            }
+          
+            repeatPassword.addEventListener("keyup", (e) => {
+              matchTest()
+              })
+            }
+        </script>    
+     <div class="form">
+       <form id="register-form" action="#">
           <ul class="form-container">
             <li>
               <h2>Create Account</h2>
-            </li> 
+            </li>
             <li>
               <label for="name">Name</label>
-              <input
-                type="name"
-                name="name" 
-                required
-                id="name" />
+              <input type="name" name="name" id="name" required />
             </li>
             <li>
-              <label for="email">Email</label>
-              <input 
-                type="email"
-                name="email"
-                id="email"   
-                required />
+              <label for="email" class="emailBox">Email</label>
+              <input type="email" name="email" id="email" required />
+              <span class="emailText"></span>
             </li>
             <li>
-              <label for="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                required        
-                />
+              <label for="password" class="passBox">Password</label>
+              <input type="password" id="password" name="password" class="password" required />
+              <span class="passText"></span>
             </li>
             <li>
               <label for="re-password">Re-Enter Password</label>
-              <input
-                type="password"
-                id="re-password"
-                name="re-password"
-                onkeyup="matchTest()"
-                required            
-                />
+              <input type="password" id="re-password" name="re-password" class="re-password" required />
             </li>
             <li>
               <button type="submit" class="primary">
-                Register
+              Register
               </button>
             </li>
             <li>
@@ -141,6 +121,6 @@ const RegisterScreen = {
             </li>
           </ul>
         </form>
-      </div>`,
+    </div>`,
 }
 export default RegisterScreen
